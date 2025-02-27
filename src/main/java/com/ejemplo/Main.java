12c7487305d12809915758790e865d4824e9c799
Miguel Savignano <m.savignano@stuart.com>
2025-02-27 19:06:00 +0100

    test mysql connection

---
+++ src/main/java/com/ejemplo/Main.java
@@ -0,0 +1,22 @@
+package com.ejemplo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+  public static void main(String[] args) {
+    String url = "jdbc:mysql://aws.connect.psdb.cloud:3306/test_database";
+    String usuario = "root";
+    String contraseña = "password";
+
+    try {
+      Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
+      System.out.println("✅ Conexión exitosa");
+      conexion.close();
+    } catch (SQLException e) {
+      System.out.println("❌ No se pudo conectar");
+      e.printStackTrace();
+    }
+  }
+}
