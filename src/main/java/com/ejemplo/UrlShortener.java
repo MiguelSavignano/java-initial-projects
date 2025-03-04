3d8095a3eecde727666de372782a198e2e76362c
Miguel Savignano <m.savignano@stuart.com>
2025-03-04 19:35:57 +0100

    random uuid

--- src/main/java/com/ejemplo/UrlShortener.java
+++ src/main/java/com/ejemplo/UrlShortener.java
@@ -1,23 +1,9 @@
 package com.ejemplo;
 
-import java.util.Random;
+import java.util.UUID;
 
 public class UrlShortener {
-
-  // Método para generar un identificador aleatorio para la URL corta
   public static String generateRandomShortUrl() {
-    // Caracteres posibles para la URL corta
-    String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    Random random = new Random();
-    StringBuilder shortUrl = new StringBuilder();
-
-    // Generar una cadena de 8 caracteres aleatorios
-    for (int i = 0; i < 8; i++) {
-      int index = random.nextInt(characters.length());
-      shortUrl.append(characters.charAt(index));
-    }
-
-    // Retornar la URL corta como "http://short.url/{identificador_aleatorio}"
-    return "http://short.url/" + shortUrl.toString();
+    return UUID.randomUUID().toString().substring(0, 8);
   }
 }
