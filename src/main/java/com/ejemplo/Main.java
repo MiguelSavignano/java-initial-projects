8cf96d3771f7bf2e129134f4f1d4d5676c30a2c6
Alex <you@example.com>
2025-03-13 20:14:54 +0100

    .

--- src/main/java/com/ejemplo/Main.java
+++ src/main/java/com/ejemplo/Main.java
@@ -1,9 +1,23 @@
 package com.ejemplo;
 
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    CalcularMedia.run();
-    //BuscarDuplicados.run();
+    String opcion= "";
+    Scanner leer = new Scanner(System.in);
+    opcion = leer.nextLine();
+    System.out.println("Que operacion quuieres hacer? 1 o 2");
+
+    System.out.println(opcion);
+
+    if ("1".equals(opcion)) {
+      CalcularMedia.run();
+      System.out.println("opcion 1");
+    } else if ("2".equals(opcion)) { 
+      System.out.println("opcion 2");
+      BuscarDuplicados.run();
+    }
+
   }
 }
