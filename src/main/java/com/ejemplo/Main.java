385af9c41989610c802b3f02feee3e3178a15581
Alex <you@example.com>
2025-03-18 20:13:09 +0100

    calculadora suma multiple

--- src/main/java/com/ejemplo/Main.java
+++ src/main/java/com/ejemplo/Main.java
@@ -4,38 +4,24 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    String opcion= "";
     Scanner leer = new Scanner(System.in);
-    opcion = leer.nextLine();
-    System.out.println("Que operacion quuieres hacer? 1 o 2");
-
-    System.out.println(opcion);
-
-    if ("1".equals(opcion)) {
-      CalcularMedia.run();
-      System.out.println("opcion 1");
-
-    } else if ("2".equals(opcion)) { 
-      System.out.println("opcion 2");
-      BuscarDuplicados.run();
+    String operacion= "";
+    int resultado = 0;
+    while (true) {
       
-    } else if ("3".equals(opcion)) { 
-      System.out.println("opcion 3"); 
+
+      System.out.println("Que operacion quuieres hacer? +,*");
+      operacion = leer.next();
+      System.out.println(operacion); 
+
+
+      if ("+".equals(operacion)) {
+        System.out.println("numero: ");
       int numero1 = leer.nextInt();
-      int numero2 = leer.nextInt();
-      Sumar.run(numero1, numero2);
-      
-    } else if ("4".equals(opcion)) { 
-      System.out.println("opcion 4");
-      BuscarDuplicados.run();
-      
-    } else if ("5".equals(opcion)) { 
-      System.out.println("opcion 5");
-      BuscarDuplicados.run();
-      
+        System.out.println("operacion sumar"); 
+        resultado = Sumar.run(resultado, numero1);
+      }
     }
-
-
   }
 
 }
--- src/main/java/com/ejemplo/Sumar.java
+++ src/main/java/com/ejemplo/Sumar.java
@@ -2,7 +2,9 @@ package com.ejemplo;
 
 public class Sumar {
 
-    public static void run(int numero1, int numero2) {
-    System.out.println(numero1+numero2);
+    public static int run(int numero1, int numero2) {
+        System.out.println("el resultado es");
+        System.out.println(numero1+numero2); 
+    return numero1 + numero2;
     }
 }
