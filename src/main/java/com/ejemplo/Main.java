850a2e29eaddf395be33676d0a8ac8e6f18e2b1e
Alex <you@example.com>
2025-03-18 20:40:29 +0100

    Version final calculadora

--- src/main/java/com/ejemplo/Main.java
+++ src/main/java/com/ejemplo/Main.java
@@ -10,7 +10,7 @@ public class Main {
     while (true) {
       
 
-      System.out.println("Que operacion quuieres hacer? +,*");
+      System.out.println("Que operacion quieres hacer? +,*");
       operacion = leer.next();
       System.out.println(operacion); 
 
@@ -32,11 +32,12 @@ public class Main {
         int numero1 = leer.nextInt();
         System.out.println("operacion Multiplicar");
       resultado = Multiplicar.run(resultado, numero1);
-     } else if ("multiplicar".equals(operacion)) {
+     } else if ("/".equals(operacion)) {
       System.out.println("numero: ");
       int numero1 = leer.nextInt();
-      System.out.println("operacion Multiplicar");
-    resultado = Multiplicar.run(resultado, numero1);
+      System.out.println("/");
+    resultado = Dividir.run(resultado, numero1);
+     }
     }
   }
 
