ea9336f023c87243fc9b6b46a0402bd033eb7bf5
Alex <you@example.com>
2025-03-25 20:43:35 +0100

    Version final Numero Magico

--- src/main/java/com/ejemplo/Main.java
+++ src/main/java/com/ejemplo/Main.java
@@ -5,30 +5,24 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     System.out.println("hola mundo");
-
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.print("Introduce un número entero: ");
-    int numeroEntero = scanner.nextInt();
-    System.out.println("Has introducido: " + numeroEntero);
-
     int numeroAleatorio = (int) (Math.random() * 11);
+    Scanner scanner = new Scanner(System.in);
+    boolean adivinado = false;
 
-    // Imprime el número aleatorio
-    System.out.println("El número aleatorio es: " + numeroAleatorio);
+    while (!adivinado) {
+      System.out.print("Introduce un número entero entre 0 y 10: ");
+      int numeroEntero = scanner.nextInt();
+      System.out.println("Has introducido: " + numeroEntero);
 
-    if(numeroEntero == numeroAleatorio) {
-      System.out.println("Ganaste");
-      
-    }else{
-      System.out.println("Perdiste");
+      if (numeroEntero == numeroAleatorio) {
+        System.out.println("¡Ganaste! Adivinaste el número.");
+        adivinado = true;
+      } else {
+        System.out.println("Incorrecto. Intenta de nuevo.");
+      }
     }
 
-
-
-
-
-
-
+    System.out.println("Gracias por jugar. ¡Hasta la próxima!");
+    scanner.close();
   }
-}    
+}
